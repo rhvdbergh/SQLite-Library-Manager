27ba84e7acda1a08774813efa3f71ebd022f8d36
Ronald van der Bergh <rhvdbergh@gmail.com>
2018-04-15 22:06:01 -0400

    add new book page and styles

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'SQLite Library Manager' });
+});
+
+/* GET new books page. */
+router.get('/books/new', function(req, res, next) {
+  res.render('new_book', { title: 'SQLite Library Manager: New Book' });
 });
 
 module.exports = router;
