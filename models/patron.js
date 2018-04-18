dee4ea191c53214ed8796306a02f76b29d6a0a24
Ronald van der Bergh <rhvdbergh@gmail.com>
2018-04-18 17:06:41 -0400

    new patron validation, library id not unique

--- models/patron.js
+++ models/patron.js
@@ -1,12 +1,37 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   var Patron = sequelize.define('Patron', {
-    first_name: DataTypes.STRING,
-    last_name: DataTypes.STRING,
-    address: DataTypes.STRING,
-    email: DataTypes.STRING,
-    library_id: DataTypes.STRING,
-    zip_code: DataTypes.INTEGER
+    first_name: {
+      type: DataTypes.STRING,
+      validate: {notEmpty: {msg: 'First name required'}}
+    },
+    last_name: {
+      type: DataTypes.STRING,
+      validate: {notEmpty: {msg: 'Last name required'}}
+    },
+    address: {
+      type: DataTypes.STRING,
+      validate: {notEmpty: {msg: 'Address required'}}
+    },
+    email: {
+      type: DataTypes.STRING,
+      validate: {is: {
+        args: /.+\@.+\..+/, // RegEx to check for email
+        msg: "Please enter a valid email address"
+      }}
+    },
+    library_id: {
+      type: DataTypes.STRING,
+      validate: {notEmpty: {msg: 'Address required'}},
+      unique: true
+    },
+    zip_code: {
+      type: DataTypes.STRING,
+      validate: {is: {
+        args: /^\d{5}$/, // RegEx to check for 5 digits
+        msg: "Please enter a zip code consisting of 5 digits"
+      }}
+    }
   }, {timestamps: false});
   Patron.associate = function(models) {
     // associations can be defined here
