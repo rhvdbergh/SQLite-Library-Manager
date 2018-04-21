0e76ff5001d31f41ed320a7a7fded9d8637d3fbf
Ronald van der Bergh <rhvdbergh@gmail.com>
2018-04-21 00:47:10 -0400

    associations change, Loan.belongsTo book and patron

--- models/loan.js
+++ models/loan.js
@@ -1,20 +1,14 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   var Loan = sequelize.define('Loan', {
-    book_id: {
-      type: DataTypes.INTEGER
-    },
-    patron_id: {
-      type:DataTypes.INTEGER
-    },
-      loaned_on: DataTypes.DATE,
+    loaned_on: DataTypes.DATE,
     return_by: DataTypes.DATE,
     returned_on: DataTypes.DATE
   }, {timestamps: false, underscored: true});
   Loan.associate = function(models) {
     // associations can be defined here
-    Loan.hasOne(models.Patron);
-    Loan.hasOne(models.Book);
+    Loan.belongsTo(models.Patron);
+    Loan.belongsTo(models.Book);
   };
   return Loan;
 };
